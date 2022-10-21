982c241c1fd63ac5b4ada993ea8ebb3b6ad8c58a
keruru0987 <793363750@qq.com>
2022-10-21 19:16:10 +1100

    initial

---
+++ assets/webpage.js
@@ -0,0 +1,41 @@
+// Side Menu
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+// Slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    transition: 500,
+    interval: 6000
+});
+
+// Scrollspy
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
+
+// Material Boxed
+const mb = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(mb, {});
+
+$(document).ready(function(){
+    $('.modal-trigger').leanModal();
+    $('.datepicker').datepicker();
+});
+
+$('select').material_select();
+
+$(".modal").modal({
+    onCloseEnd: function()
+    {
+        $('input[type=checkbox]').prop('checked',false);
+    }
+})
+
+$(".mySearchButton").click(function ()
+{
+    $(".myVenue").slideDown();
+})
+
+$(".myVenue").slideUp(0);
